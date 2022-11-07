9c97c0d4e97225a3bc8f1992db0e60404d0200d6
AnthoniaUdoba <anthonia.udoba@gmail.com>
2022-11-07 22:47:18 +0100

    third commit

--- js/app.js
+++ js/app.js
@@ -87,23 +87,20 @@ function cardClick(clickedCard) {
     handleClick.classList.contains("card") &&
     !handleClick.classList.contains("open", "show", "match")
   ) {
-    incrementMoveCount();
-    storeCard(clickedCard);
     //add classes open and show to the selected card
     handleClick.classList.add("open", "show");
-    if (openedCards.length === 2) {
-      if (openedCards[0].innerHTML === openedCards[1].innerHTML) {
-        matched();
-      } else {
-        openedCards[0].classList.add("unmatch");
-        openedCards[1].classList.add("unmatch");
-        setTimeout(notMatched, 300);
-      }
+    openedCards.push(handleClick);
+  }
+  if (openedCards.length === 2) {
+    incrementMoveCount();
+    if (openedCards[0].innerHTML === openedCards[1].innerHTML) {
+      matched();
+    } else {
+      openedCards[0].classList.add("unmatch");
+      openedCards[1].classList.add("unmatch");
+      setTimeout(notMatched, 300);
     }
   }
-}
-function storeCard(thisCard) {
-  openedCards.push(thisCard.target);
 }
 
 deck.addEventListener("click", cardClick);
@@ -130,12 +127,43 @@ function incrementMoveCount() {
   starRating(moveCounter);
 }
 
-//function to change star rating
+//function to change stare rating
+// function starRating(moveCounter) {
+//   if (moveCounter === 24 || moveCounter === 32) {
+//     let star = document.querySelector(".fa-star");
+//     //24 moves will reduce one star by deleteing the childNode of starsRating
+//     star.remove();
+//   }
+// }
+
+// Filled star icon
+const $starFill = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="orange" class="bi bi-star-fill" viewBox="0 0 16 16">
+  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+</svg>
+`;
+// Empty star icon
+const $starEmpty = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="orange" class="bi bi-star" viewBox="0 0 16 16">
+  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+</svg>
+`;
+// Ratings component
 function starRating(moveCounter) {
-  if (moveCounter === 24 || moveCounter === 32) {
-    let star = document.querySelector(".fa-star");
-    star.remove();
-  }
+  return `
+        <div class='rating d-flex align-items-center justify-content-center'>
+       
+            <span>
+                ${moveCounter > 24 ? $starFill : $starEmpty}
+            </span>
+            <span>
+            ${moveCounter >= 19 ? $starFill : $starEmpty}
+        </span>
+            <span>
+            ${moveCounter <= 18 ? $starFill : $starEmpty}
+        </span>
+        </div>
+    `;
 }
 
 //function to check if cards match
@@ -190,8 +218,10 @@ function showResult() {
   matchedCards += 2;
   if (matchedCards == 16) {
     setTimeout(function () {
-      modal.innerHTML = `<section class="score-panel"> <h1 class="you-won">YAAAAAAY! YOU WON!</h1> With
-        ${moveCounter} moves, in ${timeCounter} seconds, and ${ratings} star(s) </section> <button class="congratsPlay"> Play Again<i class="fa fa-repeat"></i></button>`;
+      modal.innerHTML = `<section class="score-panel"> <h1 class="you-won">Congratuations! You Completed the game!</h1> With
+      ${moveCounter} moves, in ${timeCounter} seconds, and ${starRating(
+        moveCounter
+      )} star(s) <button class="congratsPlay"> Play Again<i class="fa fa-repeat"></i></button></section> `;
       modal.classList.add("show");
       const play = document.getElementsByClassName("congratsPlay");
       play[0].addEventListener("click", resetGame);
